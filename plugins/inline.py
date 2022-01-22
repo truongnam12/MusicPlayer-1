391a6aad3bce0f082326404bbc6dfca4e3a76389
truongnam12 <95694405+truongnam12@users.noreply.github.com>
2022-01-23 01:41:56 +0700

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -27,15 +27,7 @@ from pyrogram import Client, errors
 from config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
-    [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-        InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
-    ],
-    [
-        InlineKeyboardButton('🎧Play Music', url=f'https://t.me/{USERNAME}'),
-        InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help')       
-    ]
-    ]
+   
 @Client.on_inline_query()
 async def search(client, query):
     answers = []
@@ -45,8 +37,7 @@ async def search(client, query):
                 title="Deploy",
                 input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/MusicPlayer) below.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
-                )
-            )
+               
         await query.answer(results=answers, cache_time=0)
         return
     string = query.query.lower().strip().rstrip()
